fdbac77857363ecaebf0ce41a11e2d0ce1165b95
kinnon <741551902@qq.com>
2022-07-08 04:28:40 +0800

    hot-fix first commit!

--- src/main/java/com/kinnon/git/GitTest.java
+++ src/main/java/com/kinnon/git/GitTest.java
@@ -7,13 +7,13 @@ package com.kinnon.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
+        System.out.println("Hello Git444!");
     }
 }
 
