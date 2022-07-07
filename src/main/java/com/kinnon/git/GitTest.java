804cf969b6fdba86bc2a67d0837c4bbbd5c0e227
kinnon <741551902@qq.com>
2022-07-08 04:13:06 +0800

    first commit!

---
+++ src/main/java/com/kinnon/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kinnon.git;
+
+/**
+ * @author Kinnon
+ * @create 2022-07-08 4:08
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
+
